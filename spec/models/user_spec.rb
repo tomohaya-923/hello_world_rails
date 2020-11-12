17536d8a9bc5493f6b652c80e600113688be34fc
トモアキ <h.tkaz15@gmail.com>
2020-11-12 16:52:20 +0900

    User modelのspecをFactoryBotを使って書き換え

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "account を指定しているとき" do
     it "ユーザーが作られる" do
-      user = User.new(name: "foo", account: "foo", email: "foo@example.com")
+      user = build(:user)
+      # user = User.new(name: "foo", account: "foo", email: "foo@example.com")
       # expect(user.valid?).to eq true
       expect(user).to be_valid
     end
@@ -11,7 +12,8 @@ RSpec.describe User, type: :model do
 
   context "account を指定していないとき" do
     it "ユーザー作成に失敗する" do
-      user = User.new(name: "foo", email: "foo@example.com")
+      user = build(:user, account: nil)
+      # user = User.new(name: "foo", email: "foo@example.com")
       expect(user).to be_invalid
       # binding.pry
       expect(user.errors.details[:account][0][:error]).to eq :blank
@@ -25,8 +27,10 @@ RSpec.describe User, type: :model do
 
   context "すでに同じ名前の account が存在しているとき" do
     it "ユーザー作成に失敗する" do
-      User.create!(name: "foo", account: "foo", email: "foo@example.com")
-      user = User.new(name: "bar", account: "foo", email: "bar@example.com")
+      create(:user, account: "foo")
+      # User.create!(name: "foo", account: "foo", email: "foo@example.com")
+      user = build(:user, account: "foo")
+      # user = User.new(name: "bar", account: "foo", email: "bar@example.com")
 
       expect(user).to be_invalid
       # binding.pry
